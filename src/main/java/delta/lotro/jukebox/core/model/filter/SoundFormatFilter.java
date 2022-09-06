e0f8245b6589986f0bbbb667b50e8a5b28dd1d5a
Damien Morcellet <dmorcellet@free.fr>
2022-09-06 23:13:34 +0200

    Added filters for sounds

---
+++ src/main/java/delta/lotro/jukebox/core/model/filter/SoundFormatFilter.java
@@ -0,0 +1,51 @@
+package delta.lotro.jukebox.core.model.filter;
+
+import delta.common.utils.collections.filters.Filter;
+import delta.lotro.jukebox.core.model.SoundDescription;
+import delta.lotro.jukebox.core.model.SoundFormat;
+
+/**
+ * Filter for sound of a given format.
+ * @author DAM
+ */
+public class SoundFormatFilter implements Filter<SoundDescription>
+{
+  private SoundFormat _format;
+
+  /**
+   * Constructor.
+   * @param format Format to select (may be <code>null</code>).
+   */
+  public SoundFormatFilter(SoundFormat format)
+  {
+    _format=format;
+  }
+
+  /**
+   * Get the format to use.
+   * @return A format or <code>null</code>.
+   */
+  public SoundFormat getCategory()
+  {
+    return _format;
+  }
+
+  /**
+   * Set the format to select.
+   * @param format Format to use, may be <code>null</code>.
+   */
+  public void setCategory(SoundFormat format)
+  {
+    _format=format;
+  }
+
+  @Override
+  public boolean accept(SoundDescription sound)
+  {
+    if (_format==null)
+    {
+      return true;
+    }
+    return sound.getFormat()==_format;
+  }
+}
---
+++ src/main/java/delta/lotro/jukebox/core/model/filter/SoundNameFilter.java
@@ -0,0 +1,67 @@
+package delta.lotro.jukebox.core.model.filter;
+
+import delta.common.utils.collections.filters.Filter;
+import delta.common.utils.text.MatchType;
+import delta.common.utils.text.StringFilter;
+import delta.lotro.jukebox.core.model.SoundDescription;
+
+/**
+ * Filter for sound name.
+ * @author DAM
+ */
+public class SoundNameFilter implements Filter<SoundDescription>
+{
+  private StringFilter _filter;
+  private String _pattern;
+
+  /**
+   * Constructor.
+   */
+  public SoundNameFilter()
+  {
+    this("");
+  }
+
+  /**
+   * Constructor.
+   * @param pattern String filter for name.
+   */
+  public SoundNameFilter(String pattern)
+  {
+    _filter=new StringFilter("",MatchType.CONTAINS,true);
+    _pattern=pattern;
+  }
+
+  /**
+   * Get the pattern to use to filter command.
+   * @return A pattern string.
+   */
+  public String getPattern()
+  {
+    return _pattern;
+  }
+
+  /**
+   * Set the string pattern.
+   * @param pattern Pattern to set.
+   */
+  public void setPattern(String pattern)
+  {
+    if (pattern==null)
+    {
+      pattern="";
+    }
+    _pattern=pattern;
+    _filter=new StringFilter(pattern,MatchType.CONTAINS,true);
+  }
+
+  public boolean accept(SoundDescription sound)
+  {
+    String command=sound.getName();
+    if (command!=null)
+    {
+      return _filter.accept(command);
+    }
+    return false;
+  }
+}
